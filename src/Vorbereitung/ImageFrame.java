996e33d8d5db9597c56653c416d444710d987550
DarkMio <mio@zier.ws>
2015-10-09 14:19:03 +0200

    shortened code / removed files

--- src/Vorbereitung/ImageFrame.java
+++ src/Vorbereitung/ImageFrame.java
@@ -33,7 +33,6 @@ public class ImageFrame extends JFrame implements ActionListener {
                 File f = new File(fc.getSelectedFile().getAbsolutePath());
                 BufferedImage bi = ip.getImg();
                 ImageIO.write(bi, "png", f);
-
             } catch (IOException e){
                 e.printStackTrace();
             }
