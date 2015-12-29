647be9d31610a1e3df2b47594b75d8a42b9fb140
DarkMio <mio@zier.ws>
2015-12-29 16:39:55 +0100

    Holy fucking jesus christ. Mat4x4 + Transform are in and optimized.

---
+++ src/MathFunc/Mat4x4.java
@@ -0,0 +1,171 @@
+package MathFunc;
+
+
+public class Mat4x4 {
+
+    public final double m11;
+    public final double m12;
+    public final double m13;
+    public final double m14;
+    public final double m21;
+    public final double m22;
+    public final double m23;
+    public final double m24;
+    public final double m31;
+    public final double m32;
+    public final double m33;
+    public final double m34;
+    public final double m41;
+    public final double m42;
+    public final double m43;
+    public final double m44;
+    public final double determinant;
+
+
+    public Mat4x4(double m11, double m12, double m13, double m14,
+                  double m21, double m22, double m23, double m24,
+                  double m31, double m32, double m33, double m34,
+                  double m41, double m42, double m43, double m44) {
+        this.m11 = m11;
+        this.m12 = m12;
+        this.m13 = m13;
+        this.m14 = m14;
+        this.m21 = m21;
+        this.m22 = m22;
+        this.m23 = m23;
+        this.m24 = m24;
+        this.m31 = m31;
+        this.m32 = m32;
+        this.m33 = m33;
+        this.m34 = m34;
+        this.m41 = m41;
+        this.m42 = m42;
+        this.m43 = m43;
+        this.m44 = m44;
+        determinant = m11 * m22 * m33 * m44
+                    + m12 * m23 * m34 * m41
+                    + m13 * m24 * m31 * m42
+                    + m14 * m21 * m32 * m43
+                    - m41 * m32 * m23 * m14
+                    - m42 * m33 * m24 * m11
+                    - m43 * m34 * m21 * m12
+                    - m44 * m31 * m22 * m13;
+
+    }
+
+    public Mat4x4 mul(Vector3 o) { // o = other
+        if (o == null) throw new IllegalArgumentException("Parameter must not be null");
+        return new Mat4x4(m11 * o.x, m12 * o.y, m13 * o.z, 0,
+                          m21 * o.x, m22 * o.y, m23 * o.z, 0,
+                          m31 * o.x, m32 * o.y, m33 * o.z, 0,
+                          m41 * o.x, m42 * o.y, m43 * o.z, 0);
+    }
+
+    public Mat4x4 mul(Point3 o) { // o = other
+        if (o == null) throw new IllegalArgumentException("Parameter must not be null");
+        return new Mat4x4(m11 * o.x, m12 * o.y, m13 * o.z, m14,
+                          m21 * o.x, m22 * o.y, m23 * o.z, m24,
+                          m31 * o.x, m32 * o.y, m33 * o.z, m34,
+                          m41 * o.x, m42 * o.y, m43 * o.z, m44);
+    }
+
+    public Mat4x4 mul(Mat4x4 o) { // o = other, n = new
+        if (o == null) throw new IllegalArgumentException("Parameter must not be null");
+        // Following segment is easier to debug (probably not needed tho)
+        final double n11 = m11 * o.m11 + m12 * o.m21 + m13 * o.m31 + m14 * o.m41;
+        final double n12 = m11 * o.m12 + m12 * o.m22 + m13 * o.m32 + m14 * o.m42;
+        final double n13 = m11 * o.m13 + m12 * o.m23 + m13 * o.m33 + m14 * o.m43;
+        final double n14 = m11 * o.m14 + m12 * o.m24 + m13 * o.m34 + m14 * o.m44;
+        final double n21 = m21 * o.m11 + m22 * o.m21 + m23 * o.m31 + m24 * o.m41;
+        final double n22 = m21 * o.m12 + m22 * o.m22 + m23 * o.m32 + m24 * o.m42;
+        final double n23 = m21 * o.m13 + m22 * o.m23 + m23 * o.m33 + m24 * o.m43;
+        final double n24 = m21 * o.m14 + m22 * o.m24 + m23 * o.m34 + m24 * o.m44;
+        final double n31 = m31 * o.m11 + m32 * o.m21 + m33 * o.m31 + m34 * o.m41;
+        final double n32 = m31 * o.m12 + m32 * o.m22 + m33 * o.m32 + m34 * o.m42;
+        final double n33 = m31 * o.m13 + m32 * o.m23 + m33 * o.m33 + m34 * o.m43;
+        final double n34 = m31 * o.m14 + m32 * o.m24 + m33 * o.m34 + m34 * o.m44;
+        final double n41 = m41 * o.m11 + m42 * o.m21 + m43 * o.m31 + m44 * o.m41;
+        final double n42 = m41 * o.m12 + m42 * o.m22 + m43 * o.m32 + m44 * o.m42;
+        final double n43 = m41 * o.m13 + m42 * o.m23 + m43 * o.m33 + m44 * o.m43;
+        final double n44 = m41 * o.m14 + m42 * o.m24 + m43 * o.m34 + m44 * o.m44;
+        return new Mat4x4(n11, n12, n13, n14,
+                          n21, n22, n23, n24,
+                          n31, n32, n33, n34,
+                          n41, n42, n43, n44);
+    }
+
+    public Mat4x4 transpose() {
+        return new Mat4x4(m11, m21, m31, m41,
+                          m12, m22, m32, m42,
+                          m13, m23, m33, m43,
+                          m14, m24, m34, m44);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mat4x4 mat4x4 = (Mat4x4) o;
+
+        if (Double.compare(mat4x4.m11, m11) != 0) return false;
+        if (Double.compare(mat4x4.m12, m12) != 0) return false;
+        if (Double.compare(mat4x4.m13, m13) != 0) return false;
+        if (Double.compare(mat4x4.m14, m14) != 0) return false;
+        if (Double.compare(mat4x4.m21, m21) != 0) return false;
+        if (Double.compare(mat4x4.m22, m22) != 0) return false;
+        if (Double.compare(mat4x4.m23, m23) != 0) return false;
+        if (Double.compare(mat4x4.m24, m24) != 0) return false;
+        if (Double.compare(mat4x4.m31, m31) != 0) return false;
+        if (Double.compare(mat4x4.m32, m32) != 0) return false;
+        if (Double.compare(mat4x4.m33, m33) != 0) return false;
+        if (Double.compare(mat4x4.m34, m34) != 0) return false;
+        if (Double.compare(mat4x4.m41, m41) != 0) return false;
+        if (Double.compare(mat4x4.m42, m42) != 0) return false;
+        if (Double.compare(mat4x4.m43, m43) != 0) return false;
+        if (Double.compare(mat4x4.m44, m44) != 0) return false;
+        return Double.compare(mat4x4.determinant, determinant) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(m11);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m12);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m13);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m14);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m21);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m22);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m23);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m24);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m31);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m32);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m33);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m34);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m41);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m42);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m43);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(m44);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(determinant);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
---
+++ src/Raytracing/Transform.java
@@ -0,0 +1,192 @@
+package Raytracing;
+
+import MathFunc.Mat4x4;
+import MathFunc.Vector3;
+
+import java.util.Random;
+
+public class Transform {
+
+    public final Mat4x4 m;
+    public final Mat4x4 i;
+
+    public Transform() {
+        m = new Mat4x4(1, 0, 0, 0,
+                       0, 1, 0, 0,
+                       0, 0, 1, 0,
+                       0, 0, 0, 1);
+        i = new Mat4x4(1, 0, 0, 0,
+                       0, 1, 0, 0,
+                       0, 0, 1, 0,
+                       0, 0, 0, 1);
+    }
+
+    private Transform(Mat4x4 m, Mat4x4 i) {
+        this.m = m;
+        this.i = i;
+    }
+
+    /**
+     * You have to trust me with this code segment.
+     * Translating a matrix is actually pretty easy,
+     * imagine following scenario, you have following 4x4 matrix:
+     *   | a  b  c  d
+     * --+------------
+     * w | wa wb wc wd
+     * x | xa xb xc xd
+     * y | ya yb yc yd
+     * z | za zb zc zd
+     *
+     * then we just have to do the multiplication for following positions:
+     * wd, xd, yd, zd - which are copy pasted from the Mat4x4 class.
+     * Since the Mat4x4 is tested (a bit, hopefully, maybe) this should work fine and
+     * is about ~4 times faster than doing all unnecessary multiplications.
+     *
+     * And before you have to debug it:
+     *  - tMat -> transform matrix
+     *  - iMat -> inverse transform matrix
+     *
+     * @param v Vector3 to do the translation on
+     * @return transform matrix and inverse transformation matrix
+     */
+    public Transform translate(Vector3 v) {
+        final double tMatW = m.m11 * v.x + m.m12 * v.y + m.m13 * v.z + m.m14;
+        final double tMatX = m.m21 * v.x + m.m22 * v.y + m.m23 * v.z + m.m24;
+        final double tMatY = m.m31 * v.x + m.m32 * v.y + m.m33 * v.z + m.m34;
+        final double tMatZ = m.m41 * v.x + m.m42 * v.y + m.m43 * v.z + m.m44;
+        final Mat4x4 tMat = new Mat4x4(m.m11, m.m12, m.m13, tMatW,
+                                       m.m21, m.m22, m.m23, tMatX,
+                                       m.m31, m.m32, m.m33, tMatY,
+                                       m.m41, m.m42, m.m43, tMatZ);
+        final Vector3 in = v.mul(-1);
+        final double iMatW = i.m11 * in.x + i.m12 * in.y + i.m13 * in.z + i.m14;
+        final double iMatX = i.m21 * in.x + i.m22 * in.y + i.m23 * in.z + i.m24;
+        final double iMatY = i.m31 * in.x + i.m32 * in.y + i.m33 * in.z + i.m34;
+        final double iMatZ = i.m41 * in.x + i.m42 * in.y + i.m43 * in.z + i.m44;
+        final Mat4x4 iMat = new Mat4x4(m.m11, m.m12, m.m13, iMatW,
+                                       m.m21, m.m22, m.m23, iMatX,
+                                       m.m31, m.m32, m.m33, iMatY,
+                                       m.m41, m.m42, m.m43, iMatZ);
+        return new Transform(tMat, iMat);
+    }
+
+    /**
+     * This case isn't as easy - but still possible to shortcut it by a bit
+     * Since we're changing the identity matrix like this:
+     *   | a  b  c  d
+     * --+------------
+     * w | vx
+     * x |    vy
+     * y |       vz
+     * z |          1
+     * It is only necessary to multiply all values within one row with
+     * the value of the vector - or leave it, in case of row d.
+     * @param v
+     * @return
+     */
+    public Transform scale(Vector3 v) {
+        final Mat4x4 tMat = new Mat4x4(m.m11 * v.x, m.m12 * v.y, m.m13 * v.z, m.m14,
+                                       m.m21 * v.x, m.m22 * v.y, m.m23 * v.z, m.m24,
+                                       m.m31 * v.x, m.m32 * v.y, m.m33 * v.z, m.m34,
+                                       m.m41 * v.x, m.m42 * v.y, m.m43 * v.z, m.m44);
+        final Mat4x4 iMat = new Mat4x4(i.m11 * 1/v.x, i.m12 * 1/v.y, i.m13 * 1/v.z, i.m14,
+                                       i.m21 * 1/v.x, i.m22 * 1/v.y, i.m23 * 1/v.z, i.m24,
+                                       i.m31 * 1/v.x, i.m32 * 1/v.y, i.m33 * 1/v.z, i.m34,
+                                       i.m41 * 1/v.x, i.m42 * 1/v.y, i.m43 * 1/v.z, i.m44);
+        return new Transform(tMat, iMat);
+    }
+
+    /**
+     *   | a  b  c  d
+     * --+------------
+     * w | 1
+     * x |    an an
+     * y |    an an
+     * z |          1
+     * @param angle
+     * @return
+     */
+    public Transform rotateX(double angle) {
+        final double sin = Math.sin(angle);
+        final double cos = Math.cos(angle);
+        final Mat4x4 tMat = new Mat4x4(m.m11, m.m11 * cos + m.m12 * sin, m.m11 * -sin + m.m12 * cos, m.m14,
+                                       m.m21, m.m21 * cos + m.m22 * sin, m.m21 * -sin + m.m22 * cos, m.m24,
+                                       m.m31, m.m31 * cos + m.m32 * sin, m.m31 * -sin + m.m32 * cos, m.m34,
+                                       m.m41, m.m41 * cos + m.m32 * sin, m.m41 * -sin + m.m42 * cos, m.m44);
+        final Mat4x4 iMat = new Mat4x4(i.m11, i.m11 * cos + i.m12 * -sin, i.m11 * sin + i.m12 * cos, i.m14,
+                                       i.m21, i.m21 * cos + i.m22 * -sin, i.m21 * sin + i.m22 * cos, i.m24,
+                                       i.m31, i.m31 * cos + i.m32 * -sin, i.m31 * sin + i.m32 * cos, i.m34,
+                                       i.m41, i.m41 * cos + i.m32 * -sin, i.m41 * sin + i.m42 * cos, i.m44);
+        return new Transform(tMat, iMat);
+    }
+
+    /**
+     *   | a  b  c  d
+     * --+------------
+     * w | an    an
+     * x |    1
+     * y | an    an
+     * z |          1
+     * @param angle
+     * @return
+     */
+    public Transform rotateY(double angle) {
+        final double sin = Math.sin(angle);
+        final double cos = Math.cos(angle);
+        final Mat4x4 tMat = new Mat4x4(m.m11 * cos + m.m13 * -sin, m.m12, m.m11 * sin + m.m13 * cos, m.m14,
+                                       m.m21 * cos + m.m23 * -sin, m.m22, m.m21 * sin + m.m23 * cos, m.m24,
+                                       m.m31 * cos + m.m33 * -sin, m.m32, m.m31 * sin + m.m33 * cos, m.m34,
+                                       m.m41 * cos + m.m43 * -sin, m.m42, m.m41 * sin + m.m43 * cos, m.m44);
+        final Mat4x4 iMat = new Mat4x4(m.m11 * cos + m.m13 * sin, m.m12, m.m11 * -sin + m.m13 * cos, m.m14,
+                                       m.m21 * cos + m.m23 * sin, m.m22, m.m21 * -sin + m.m23 * cos, m.m24,
+                                       m.m31 * cos + m.m33 * sin, m.m32, m.m31 * -sin + m.m33 * cos, m.m34,
+                                       m.m41 * cos + m.m43 * sin, m.m42, m.m41 * -sin + m.m43 * cos, m.m44);
+        return new Transform(tMat, iMat);
+    }
+
+    /**
+     *   | a  b  c  d
+     * --+------------
+     * w | an an
+     * x | an an
+     * y |       1
+     * z |          1
+     * @param angle
+     * @return
+     */
+    public Transform rotateZ(double angle) {
+        final double sin = Math.sin(angle);
+        final double cos = Math.cos(angle);
+        final Mat4x4 tMat = new Mat4x4(m.m11 * cos + m.m12 * sin, m.m11 * -sin + m.m12 * cos, m.m13, m.m14,
+                                       m.m21 * cos + m.m22 * sin, m.m21 * -sin + m.m22 * cos, m.m13, m.m14,
+                                       m.m31 * cos + m.m32 * sin, m.m31 * -sin + m.m32 * cos, m.m33, m.m44,
+                                       m.m41 * cos + m.m42 * sin, m.m41 * -sin + m.m42 * cos, m.m33, m.m44);
+        final Mat4x4 iMat = new Mat4x4(i.m11 * cos + i.m12 * -sin, i.m11 * sin + i.m12 * cos, i.m13, i.m14,
+                                       i.m21 * cos + i.m22 * -sin, i.m21 * sin + i.m22 * cos, i.m13, i.m14,
+                                       i.m31 * cos + i.m32 * -sin, i.m31 * sin + i.m32 * cos, i.m33, i.m44,
+                                       i.m41 * cos + i.m42 * -sin, i.m41 * sin + i.m42 * cos, i.m33, i.m44);
+        return new Transform(tMat, iMat);
+    }
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transform transform = (Transform) o;
+
+        if (!m.equals(transform.m)) return false;
+        return i.equals(transform.i);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = m.hashCode();
+        result = 31 * result + i.hashCode();
+        return result;
+    }
+}
