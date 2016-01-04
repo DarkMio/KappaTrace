c9ec1f30b65c01d7ecf42eb39a8edf933f015c88
Fabian Wendland <s56503@beuth-hochschule.de>
2016-01-04 13:55:57 +0100

    OBJLoader, incomplete

---
+++ src/Raytracing/Geometry/ShapeFromFile.java
@@ -0,0 +1,86 @@
+package Raytracing.Geometry;
+
+
+import MathFunc.Point3;
+import Raytracing.*;
+import Raytracing.Material.Material;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Fabby on 03.01.2016.
+ */
+public class ShapeFromFile extends Geometry {
+
+    public final ArrayList<Geometry> objects;
+
+    public ShapeFromFile(String path, Material material) {
+        super(material);
+        objects = new ArrayList<>();
+        try {
+            System.out.println("load");
+            parseFile(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Hit hit(Ray r) {
+        double t = Double.MAX_VALUE;
+        Hit h = null;
+        for (Geometry g : objects) {
+            final Hit hit = g.hit(r);
+            if (hit != null && hit.t < t && hit.t > Epsilon.PRECISION) {
+                t = hit.t;
+                h = hit;
+            }
+        }
+        return h;
+    }
+
+    public void parseFile(File f) throws IOException {
+
+        ArrayList<Point3> points = new ArrayList<>();
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String str = s.nextLine();
+            String [] astra = str.split(" ");
+            if (str.startsWith("v")) {
+                if (astra.length == 4) {
+                    points.add(new Point3(Double.valueOf(astra[1]), Double.valueOf(astra[2]), Double.valueOf(astra[3])));
+                }
+            }
+            if (str.startsWith("f")) {
+                if(astra.length == 4) {
+                    int ab = Integer.valueOf(astra[1])-1;
+                    int b = Integer.valueOf(astra[2])-1;
+                    int c = Integer.valueOf(astra[3])-1;
+                    Point3 p1 = points.get(ab);
+                    Point3 p2 = points.get(b);
+                    Point3 p3 = points.get(c);
+                    objects.add(new Triangle (material,p1,p2,p3));
+                    System.out.println("new triangle");
+
+                }
+                if(astra.length == 5) {
+                    int ab = Integer.valueOf(astra[1])-1;
+                    int b = Integer.valueOf(astra[2])-1;
+                    int c = Integer.valueOf(astra[3])-1;
+                    int d = Integer.valueOf(astra[4])-1;
+                    Point3 p1 = points.get(ab);
+                    Point3 p2 = points.get(b);
+                    Point3 p3 = points.get(c);
+                    Point3 p4 = points.get(d);
+                    objects.add(new Triangle (material,p1,p2,p3));
+                    objects.add(new Triangle (material,p2,p3,p4));
+                    System.out.println("new triangle");
+                    //this is ghetto and work in progress, dun hate bra.
+                }
+            }
+        }
+    }
+}
--- src/main.java
+++ src/main.java
@@ -11,6 +11,7 @@ import Raytracing.Light.Light;
 import Raytracing.Light.PointLight;
 import Raytracing.Material.LambertMaterial;
 import Raytracing.Transform;
+import Raytracing.UI.Raytracer;
 import Raytracing.World;
 
 import java.util.ArrayList;
@@ -57,21 +58,24 @@ public class main {
         PerspectiveCamera ppc = new PerspectiveCamera(new Point3(8,8,8), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI/4);
         FishEyeCamera fec = new FishEyeCamera(new Point3(0,0,6), new Vector3(-0.3, -0.1, -0.3), new Vector3(0.10, 1, 0), 2, 100);
 
-//      new Raytracer(320, 240, new World(background, scene_reflective, ambientLight, lights), fec);
+//      new Raytracer(640, 480, new World(background, scene_reflective, ambientLight, lights), fec);
 //      new Raytracer(640, 480, new World(background, scene_lambert, ambientLight, lights), ppc);
 //      new Raytracer(640, 480, new World(background, scene_phong, ambientLight, lights), ppc);
 
         ArrayList<Geometry> scene2 = new ArrayList<>(Arrays.asList(
-                new Plane(new LambertMaterial(new Color(0.8, 0.8, 0.8)), new Point3(0, 0, 0), new Normal3(0, 1, 0)),
-                new AxisAlignedBox(Materials.YELLOW_REFLECTIVE,new Point3(-0.5,0,-0.5),new Point3(0.5,1,0.5))
+              //  new Plane(new LambertMaterial(new Color(0.8, 0.8, 0.8)), new Point3(0, 0, 0), new Normal3(0, 1, 0)),
+              //  new AxisAlignedBox(Materials.YELLOW_REFLECTIVE,new Point3(-0.5,0,-0.5),new Point3(0.5,1,0.5))
+               new ShapeFromFile("./src/Objects/bunny.obj", Materials.ORANGE_REFLECTIVE)
         ));
         ArrayList<Light> lights2 = new ArrayList<>();
         lights2.add(new PointLight(new Point3(8.0,8.0,0.0), new Color(1.0,1.0,1.0),true));
-        PerspectiveCamera ppc2 = new PerspectiveCamera(new Point3(8,8,8), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI/4);
-        new MultiRaytracer(640, 480, new World(background, scene2, ambientLight, lights2), ppc2,8);
+        PerspectiveCamera ppc2 = new PerspectiveCamera(new Point3(1,1,1), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI/8);
+        //new MultiRaytracer(640, 480, new World(background, scene2, ambientLight, lights2), ppc2,8);
 
         Transform x = new Transform().scale(new Vector3(3, 1, 1.5));
         ArrayList<Geometry> scene3 = new ArrayList<>(Arrays.asList(new Node(Materials.RED_REFLECTIVE, x,scene_reflective)));
-//      new Raytracer(640, 480, new World(background, scene3, ambientLight, lights2), ppc2);
+          new Raytracer(640, 480, new World(background, scene2, ambientLight, lights2), ppc2);
+//        new Raytracer(640, 480, new World(background, null, ambientLight, lights2), ppc2);
+
     }
 }
