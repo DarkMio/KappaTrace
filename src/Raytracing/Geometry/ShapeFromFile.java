ab67489ed20dac966ffdcf56bdcf5f52bb0417a9
PancakeNinja <Fabian.Wendland@googlemailcom>
2016-01-07 20:42:59 +0100

    Added commentary, checked out the shenanigans. No Broncos this time, but at least christmas is coming up soon.

--- src/Raytracing/Geometry/ShapeFromFile.java
+++ src/Raytracing/Geometry/ShapeFromFile.java
@@ -8,6 +8,7 @@ import Raytracing.Material.Material;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -72,7 +73,7 @@ public class ShapeFromFile extends Geometry {
                     continue;
                 }
             }
-            if (str.startsWith("vt")){ //broken
+            if (str.startsWith("vt")) { //broken
                 double[] doubleArray = new double[2];
                 doubleArray[0] = Double.valueOf(astra[1]);
                 doubleArray[1] = Double.valueOf(astra[2]);
@@ -80,44 +81,8 @@ public class ShapeFromFile extends Geometry {
             }
 
             if (str.startsWith("f")) {
-                System.out.println(astra[1].split("/+").length);
-                if (astra.length == 4) {
-                    if (astra[1].split("/").length == 2){ //v vt, broken
-                        int[] intArray = new int[6];
-                        intArray[0] = Integer.valueOf(astra[1].split("/")[0]) - 1;
-                        intArray[1] = Integer.valueOf(astra[2].split("/")[0]) - 1;
-                        intArray[2] = Integer.valueOf(astra[3].split("/")[0]) - 1;
-                        faces3.add(intArray);
-                    }
-                    else if (astra[1].split("//").length == 2) {  //v vn
-                        int[] intArray = new int[6];
-                        intArray[0] = Integer.valueOf(astra[1].split("//")[0]) - 1;
-                        intArray[1] = Integer.valueOf(astra[2].split("//")[0]) - 1;
-                        intArray[2] = Integer.valueOf(astra[3].split("//")[0]) - 1;
-                        intArray[3] = Integer.valueOf(astra[1].split("//")[1]) - 1;
-                        intArray[4] = Integer.valueOf(astra[2].split("//")[1]) - 1;
-                        intArray[5] = Integer.valueOf(astra[3].split("//")[1]) - 1;
-                        faces3.add(intArray);
-                    } else if (astra[1].split("/+").length == 3){ //v vn vt, broken
-                        int[] intArray = new int[9];
-                        intArray[0] = Integer.valueOf(astra[1].split("/")[0]) - 1;
-                        intArray[1] = Integer.valueOf(astra[2].split("/")[0]) - 1;
-                        intArray[2] = Integer.valueOf(astra[3].split("/")[0]) - 1;
-                        intArray[3] = Integer.valueOf(astra[1].split("/")[1]) - 1;
-                        intArray[4] = Integer.valueOf(astra[2].split("/")[1]) - 1;
-                        intArray[5] = Integer.valueOf(astra[3].split("/")[1]) - 1;
-                        intArray[6] = Integer.valueOf(astra[1].split("/")[2]) - 1;
-                        intArray[7] = Integer.valueOf(astra[2].split("/")[2]) - 1;
-                        intArray[8] = Integer.valueOf(astra[3].split("/")[2]) - 1;
-                        faces3.add(intArray);
-                    } else { //v
-                        int[] intArray = new int[3];
-                        intArray[0] = Integer.valueOf(astra[1].split("//")[0]) - 1;
-                        intArray[1] = Integer.valueOf(astra[2].split("//")[0]) - 1;
-                        intArray[2] = Integer.valueOf(astra[3].split("//")[0]) - 1;
-                        faces3.add(intArray);
-                    }
-                }
+                processF(astra, faces3);
+                /*System.out.println(astra[1].split("/+").length);
                 if (astra.length == 5) {
                     /*int ab = Integer.valueOf(astra[1]) - 1;
                     int b = Integer.valueOf(astra[2]) - 1;
@@ -129,31 +94,101 @@ public class ShapeFromFile extends Geometry {
                     Point3 p4 = points.get(d);
                     objects.add(new Triangle(material, p1, up, p2, up, p3, up));
                     objects.add(new Triangle(material, p1, up, p3, up, p4, up));
-                    System.out.println("new poly");*/
+                    System.out.println("new poly");
+            }*/
+        }
+    }
+        if (!faces3.isEmpty()) {
+            if (!normals.isEmpty()) {
+                int[] check = faces3.get(0);
+                if(check.length > 6) {
+                    for (int[] a : faces3) {
+                        Point3 p1 = points.get(a[0]);
+                        Point3 p2 = points.get(a[1]);
+                        Point3 p3 = points.get(a[2]);
+                        Normal3 n1 = normals.get(a[6]);
+                        Normal3 n2 = normals.get(a[7]);
+                        Normal3 n3 = normals.get(a[8]);
+                        objects.add(new Triangle(material, p1, n1, p2, n2, p3, n3));
+                    }
+                } else if (check.length == 6) {
+                    for (int[] a : faces3) {
+                        Point3 p1 = points.get(a[0]);
+                        Point3 p2 = points.get(a[1]);
+                        Point3 p3 = points.get(a[2]);
+                        Normal3 n1 = normals.get(a[3]);
+                        Normal3 n2 = normals.get(a[4]);
+                        Normal3 n3 = normals.get(a[5]);
+                        objects.add(new Triangle(material, p1, n1, p2, n2, p3, n3));
+                    }
+                    }
+                 } else {
+                for (int[] a : faces3) {
+                    Point3 p1 = points.get(a[0]);
+                    Point3 p2 = points.get(a[1]);
+                    Point3 p3 = points.get(a[2]);
+                    objects.add(new Triangle(material, p1, n1, p2, n2, p3, n3));
                 }
             }
         }
-        if (!faces3.isEmpty()) {
-            if (!normals.isEmpty()) {
-                for (int i = 0; i < faces3.size(); i++) {
-                    int[] a = faces3.get(i);
-                    Point3 p1 = points.get(a[0]);
-                    Point3 p2 = points.get(a[1]);
-                    Point3 p3 = points.get(a[2]);
-                    Normal3 n1 = normals.get(a[3]);
-                    Normal3 n2 = normals.get(a[4]);
-                    Normal3 n3 = normals.get(a[5]);
-                    objects.add(new Triangle(material, p1, n1, p2, n2, p3, n3));
-                }
-            } else {
-                for (int i = 0; i < faces3.size(); i++) {
-                    int[] a = faces3.get(i);
-                    Point3 p1 = points.get(a[0]);
-                    Point3 p2 = points.get(a[1]);
-                    Point3 p3 = points.get(a[2]);
-                    objects.add(new Triangle(material, p1, n1, p2, n2, p3, n3));
-                }
-            }
+    }
+
+    private void processF(String[] array, ArrayList<int[]> faces3) {
+        System.out.println(array[1]);
+        if (array[1].matches("\\d//\\d")) {
+            processFacesWithN(array, faces3);
+        } else if (array[1].matches("\\d/\\d")) {
+            processFacesWithT(array, faces3);
+        } else if (array[1].matches("\\d/\\d/\\d")){
+            processFacesWithNAndT(array, faces3);
+        } else {
+            processFaces (array, faces3);
+        }
+    }
+
+    private void processFacesWithN(String[] array, ArrayList<int[]> faces3){
+        if(array.length == 4) {
+            int[] intArray = new int[6];
+            intArray[0] = Integer.valueOf(array[1].split("//")[0]) - 1;
+            intArray[1] = Integer.valueOf(array[2].split("//")[0]) - 1;
+            intArray[2] = Integer.valueOf(array[3].split("//")[0]) - 1;
+            intArray[3] = Integer.valueOf(array[1].split("//")[1]) - 1;
+            intArray[4] = Integer.valueOf(array[2].split("//")[1]) - 1;
+            intArray[5] = Integer.valueOf(array[3].split("//")[1]) - 1;
+            faces3.add(intArray);
+        }
+    }
+    private void processFacesWithT(String[] array, ArrayList<int[]> faces3) {
+        if (array.length == 4) {
+            int[] intArray = new int[3];
+            intArray[0] = Integer.valueOf(array[1].split("/")[0]) - 1;
+            intArray[1] = Integer.valueOf(array[2].split("/")[0]) - 1;
+            intArray[2] = Integer.valueOf(array[3].split("/")[0]) - 1;
+            faces3.add(intArray);
+        }
+    }
+    private void processFacesWithNAndT(String[] array, ArrayList<int[]> faces3){
+        if (array.length == 4) {
+            int[] intArray = new int[9];
+            intArray[0] = Integer.valueOf(array[1].split("/")[0]) - 1;
+            intArray[1] = Integer.valueOf(array[2].split("/")[0]) - 1;
+            intArray[2] = Integer.valueOf(array[3].split("/")[0]) - 1;
+            intArray[3] = Integer.valueOf(array[1].split("/")[1]) - 1;
+            intArray[4] = Integer.valueOf(array[2].split("/")[1]) - 1;
+            intArray[5] = Integer.valueOf(array[3].split("/")[1]) - 1;
+            intArray[6] = Integer.valueOf(array[1].split("/")[2]) - 1;
+            intArray[7] = Integer.valueOf(array[2].split("/")[2]) - 1;
+            intArray[8] = Integer.valueOf(array[3].split("/")[2]) - 1;
+            faces3.add(intArray);
+        }
+    }
+    private void processFaces (String [] array, ArrayList<int[]> faces3){
+        if (array.length == 4) {
+            int[] intArray = new int[3];
+            intArray[0] = Integer.valueOf(array[1].split("//")[0]) - 1;
+            intArray[1] = Integer.valueOf(array[2].split("//")[0]) - 1;
+            intArray[2] = Integer.valueOf(array[3].split("//")[0]) - 1;
+            faces3.add(intArray);
         }
     }
 }
